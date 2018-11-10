1da563aaedabf99d84d7193b4b5837c7ab95b363
root <18843114887@163.com>
2018-11-10 21:50:30 +0800

    Initial commit

---
+++ moudle1/src/com/hzyc/test.java
@@ -0,0 +1,18 @@
+package com.hzyc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class test {
+    //jjj
+    public static void main(String[] args) {
+
+        System.out.println("lll");
+        List<Integer> l = new ArrayList<>();
+
+    }
+
+    public void mm(){
+
+    }
+}
---
+++ src/com/hzyc/test2.java
@@ -0,0 +1,12 @@
+package com.hzyc;
+
+/**
+ * @auther sss
+ * @date 2018/11/10 - 14:13
+ */
+public class test2 {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
